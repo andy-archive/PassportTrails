fcfae65cea08e26d9b2e1b2e4f3b16893ac69746
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-10-13 10:22:59 +0900

    design: 사용자 위치 버튼 위치 및 사이즈 조정

--- PassportTrails/Utils/Extension/MKMapView+.swift
+++ PassportTrails/Utils/Extension/MKMapView+.swift
@@ -11,9 +11,9 @@ extension MKMapView {
     func configureUserTrackingButton() {
         let trackingButton = MKUserTrackingBarButtonItem(mapView: self)
         trackingButton.customView?.tintColor = .systemBlue
-        trackingButton.customView?.frame.size = CGSize(width: 50, height: 50)
+        trackingButton.customView?.frame.size = CGSize(width: 42, height: 42)
         
-        let toolBarFrame = CGRect(origin: .zero, size: CGSize(width: 50, height: 50))
+        let toolBarFrame = CGRect(origin: .zero, size: CGSize(width: 42, height: 42))
         let toolbar = UIToolbar(frame: toolBarFrame)
         toolbar.isTranslucent = false
         
@@ -27,10 +27,10 @@ extension MKMapView {
         self.addSubview(roundedSquare)
         roundedSquare.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            roundedSquare.widthAnchor.constraint(equalToConstant: 50),
-            roundedSquare.heightAnchor.constraint(equalToConstant: 50),
-            roundedSquare.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 4),
-            roundedSquare.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -30),
+            roundedSquare.widthAnchor.constraint(equalToConstant: 42),
+            roundedSquare.heightAnchor.constraint(equalToConstant: 42),
+            roundedSquare.topAnchor.constraint(equalTo: self.topAnchor, constant: 64),
+            roundedSquare.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
         ])
         
         roundedSquare.addSubview(toolbar)
