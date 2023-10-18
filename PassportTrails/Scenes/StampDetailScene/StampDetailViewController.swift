d92105bab956c147ec46b4ab6398162ba114f95e
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-10-18 18:56:33 +0900

    add: 스탬프 상세 화면에 대한 파일 생성

---
+++ PassportTrails/Scenes/StampDetailScene/StampDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StampDetailViewController.swift
+//  PassportTrails
+//
+//  Created by Taekwon Lee on 2023/10/18.
+//
+
+import UIKit
+
+final class StampDetailViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureView() {
+        super.configureView()
+    }
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+    }
+}
