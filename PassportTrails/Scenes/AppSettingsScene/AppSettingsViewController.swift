a0e9aa62598a5e15f616801bc4dba93bb9299341
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-10-27 01:22:15 +0900

    add: 설정 화면 파일 추가

---
+++ PassportTrails/Scenes/AppSettingsScene/AppSettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AppSettingsViewController.swift
+//  PassportTrails
+//
+//  Created by Taekwon Lee on 2023/10/27.
+//
+
+import UIKit
+
+final class AppSettingsViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureView() {
+        super.configureView()
+    }
+    
+    override func configureHierarchy() {
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+    }
+    
+}
