6be0f1dbb8361f287a2b7a2307176adf336064cb
Taekwon Lee <102043891+andy-archive@users.noreply.github.com>
2023-11-12 17:49:54 +0900

    feat: Push Notifications (서버 알림) 기능 구현 (#61)
    * docs: 앱 관련 파일 위치 이동 및 빌드 설정
    * feat: Apple Push Notification 기능 구현
    * chore: Firebase SDK 설치
    * feat: Firebase Messaging을 이용한 알림 기능 구현 및 테스트  성공

--- PassportTrails/Application/AppDelegate.swift
+++ PassportTrails/Application/AppDelegate.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 import RealmSwift
+import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let config = Realm.Configuration(schemaVersion: 0)
+        ///MARK: Firebase
+        FirebaseApp.configure()
         
+        ///MARK: UserNotification
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        
+        ///MARK: Firebase Messaging
+        Messaging.messaging().delegate = self
+        
+        ///MARK: RealmSwift
+        let config = Realm.Configuration(schemaVersion: 0)
         Realm.Configuration.defaultConfiguration = config
         
         return true
@@ -33,7 +52,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+///MARK: UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+//        print("Apple Push Notification token:\n", token)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+//        print("FAILED TO PUSH NOTIFICATION: \(error.localizedDescription)")
+    }
+}
+
+///MARK: MeesagingDelegate - FirebaseMessaging
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        
+        NotificationCenter.default.post(
+          name: Notification.Name("FCMToken"),
+          object: nil,
+          userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
